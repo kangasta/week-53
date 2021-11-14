883e1869ee47cde14f939f5d6bfb892074b6f19b
Toni Kangas <toni.ar.kangas@gmail.com>
2021-11-14 04:03:43 +0200

    Create presentational component for week, fix footer alignment

--- src/Routes/Week.svelte
+++ src/Routes/Week.svelte
@@ -1,44 +1,14 @@
 <script lang="ts">
-	import DayList from "../Components/DayList.svelte";
+	import Week from "../Components/Week.svelte";
 	import Navigation from "../Components/Navigation.svelte";
-	import { getWeekByMonth } from "../Utils/week";
 
 	export let year: string;
 	export let weekNumber: string;
-	export let disableNavigation = false;
-
-	$: week = getWeekByMonth(Number(year), Number(weekNumber));
-	$: months = Object.entries(week).map(([index, days]) => ({monthIndex: Number(index), days}));
 
 	$: {
-		if (!disableNavigation) {
-			document.title = `Week ${weekNumber} of ${year}`;
-		}
-
-		// December should come before January
-		if (months[0]?.monthIndex === 0 && months[1]?.monthIndex === 11) {
-			months.reverse();
-		}
+		document.title = `Week ${weekNumber} of ${year}`;
 	}
 </script>
 
-<style>
-	div.whitespace {
-		height: 2.5rem;
-		margin-bottom: 1px;
-	}
-</style>
-
-{#if !disableNavigation}
-	<h1>{year}</h1>
-{/if}
-<h2>Week {weekNumber}</h2>
-{#each months as month}
-	<DayList {...month} />
-{/each}
-{#if months.length === 1 || disableNavigation}
-	<div class="whitespace" />
-{/if}
-{#if !disableNavigation}
-	<Navigation year={Number(year)} weekNumber={Number(weekNumber)}/>
-{/if}
+<Week year={Number(year)} weekNumber={Number(weekNumber)} />
+<Navigation year={Number(year)} weekNumber={Number(weekNumber)}/>
