2a36a02194911e788b5fe836646a9a5f0edaf602
Toni Kangas <toni.ar.kangas@gmail.com>
2022-10-28 16:01:48 +0300

    Add floating navigation buttons to year view
    This allows easily navigating to top, current week, and bottom.

--- src/Routes/Year.svelte
+++ src/Routes/Year.svelte
@@ -1,7 +1,11 @@
 <script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+
+	import FloatingNavigation from "../Components/FloatingNavigation.svelte";
 	import Navigation from "../Components/Navigation.svelte";
 	import Week from "../Components/Week.svelte";
-	import { getWeeksInYear } from "../Utils/week";
+
+	import { getWeekNumber, getWeeksInYear, getWeekYear } from "../Utils/week";
 
 	export let year: string;
 
@@ -10,10 +14,33 @@
 	$: {
 		document.title = `Weeks of ${year}`;
 	};
+
+	const currentWeekId = `week-${getWeekNumber()}-of-${getWeekYear()}`;
+
+	let yearRect: DOMRect
+	let currentWeekRect: DOMRect
+
+	const updateRects = () => {
+		console.log('scroll')
+		yearRect = document.getElementById(`year-${year}`)?.getBoundingClientRect()
+		currentWeekRect = document.getElementById(currentWeekId)?.getBoundingClientRect()
+	}
+
+	onMount(() => {
+		document.addEventListener('scroll', updateRects);
+		updateRects()
+	})
+
+	onDestroy(() => {
+		document.removeEventListener('scroll', updateRects);
+	})
 </script>
 
-<h1>{year}</h1>
-{#each weekNumbers as weekNumber (`${year}-W${weekNumber}`)}
-	<Week year={Number(year)} weekNumber={weekNumber} hideYear forceBottomWhitespace />
-{/each}
-<Navigation year={Number(year)} />
+<div id={`year-${year}`}>
+	<h1>{year}</h1>
+	{#each weekNumbers as weekNumber (`${year}-W${weekNumber}`)}
+		<Week year={Number(year)} weekNumber={weekNumber} hideYear forceBottomWhitespace />
+	{/each}
+	<Navigation year={Number(year)} />
+	<FloatingNavigation yearRect={yearRect} currentWeekRect={currentWeekRect}/>
+</div>
